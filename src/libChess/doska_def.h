ab14417cda0a95df1f6959e3da0d1a46ecd1d589
Yakov Pivchik <ya_piv@mail.ru>
2021-04-17 18:19:03 +0700

    Add doska_def.h

--- src/libChess/doska_def.h
+++ src/libChess/doska_def.h
@@ -1,4 +1,15 @@
 #pragma once
 #include <iostream>
 #include <string>
+
+struct Figura {
+    string Pw, Pb;
+    Figura() : Pw{"Pw"}, Pb{"Pb"}
+    {
+    }
+};
+bool gde[9][9];
+
+string DOSKA[9][9];
+
 void doska_def();
