c49e501a98c66175c8f16084aa78719ba77a2809
Yakov Pivchik <ya_piv@mail.ru>
2021-04-17 17:53:46 +0700

    Add Chess.cpp

--- src/Chess/Chess.cpp
+++ src/Chess/Chess.cpp
@@ -1,15 +1,10 @@
+#include "libChess/gamestart.h"
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main()
 {
-    setlocale(0, "Russian");
-    doska_def();
-    int kuda = 0;
-    char otkuda = 0;
-    while (cin >> otkuda >> kuda) {
-        doska_hod(otkuda, kuda);
-    }
+    gamestart();
     return 0;
 }
