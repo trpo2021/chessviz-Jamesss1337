c850f81c12d2b8a5086debda9fd44391a89f1eb1
Пивчик Яков Геннадьевич <ip015s22@bwp12.csc.local>
2021-04-28 17:10:15 +0700

    Edit doska_hod.h

--- src/libChess/doska_hod.h
+++ src/libChess/doska_hod.h
@@ -1,8 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
-string DOSKA[9][9];
 bool gde[9][9];
 bool flag_buff = false;
 string buff;
-void doska_hod(int x, int y);
+void doska_hod(string**DOSKA,int x, int y);
