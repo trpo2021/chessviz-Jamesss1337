fe820f198878dea91ebc2fa9a85f352dfa8fc8a6
Jamesss1337 <ya_piv@mail.ru>
2021-04-29 17:36:07 +0700

    Edit gamestart.cpp

--- src/libChess/gamestart.cpp
+++ src/libChess/gamestart.cpp
@@ -11,14 +11,23 @@ string**DOSKA=new string*[9];
 for(int i=0;i<9;i++){
   DOSKA[i]=new string[9];
 }
+
+string**buff=new string*[9];
+for(int i=0;i<9;i++){
+  buff[i]=new string[9];
+}
+
+bool**gde=new bool*[9];
+for(int i=0;i<9;i++){
+  gde[i]=new bool[9];
+}
     setlocale(0, "Russian");
-    doska_def(DOSKA);
-    string buff;
+    doska_def(gde,DOSKA);
     int kuda = 0;
     char otkuda = 0;
     
     while (cin >> otkuda >> kuda) {
-        doska_hod(buff,DOSKA,otkuda, kuda);
+        doska_hod(gde,buff,DOSKA,otkuda, kuda);
     }
     return;
 }
