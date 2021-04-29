f63b59f119bc39e900c98f92f7389145cd039748
Jamesss1337 <ya_piv@mail.ru>
2021-04-29 17:35:19 +0700

    Edit doska_hod.h

--- src/libChess/doska_hod.h
+++ src/libChess/doska_hod.h
@@ -1,4 +1,4 @@
 #pragma once
 #include <iostream>
 #include <string>
-void doska_hod(std::string buff,std::string**DOSKA,int x, int y);
+void doska_hod(bool**gde,std::string**buff,std::string**DOSKA,int x, int y);
