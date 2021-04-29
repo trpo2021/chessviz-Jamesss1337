19e443bfc549287203ab4403d7cf133ea97b311a
Jamesss1337 <ya_piv@mail.ru>
2021-04-29 13:27:40 +0700

    Edit gamestart.cpp

--- src/libChess/gamestart.cpp
+++ src/libChess/gamestart.cpp
@@ -7,13 +7,18 @@ using namespace std;
 
 void gamestart()
 {
+string**DOSKA=new string*[9];
+for(int i=0;i<9;i++){
+  DOSKA[i]=new string[9];
+}
     setlocale(0, "Russian");
-    doska_def();
+    doska_def(DOSKA);
+    string buff;
     int kuda = 0;
     char otkuda = 0;
     
     while (cin >> otkuda >> kuda) {
-        doska_hod(otkuda, kuda);
+        doska_hod(buff,DOSKA,otkuda, kuda);
     }
-    return 0;
+    return;
 }
