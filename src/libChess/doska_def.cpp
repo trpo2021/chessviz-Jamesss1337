0ed98b4e30e2fad5292251ef70640bcf202e4111
Jamesss1337 <ya_piv@mail.ru.github.com>
2021-04-30 16:05:19 +0700

    Edit doska_def.cpp

--- src/libChess/doska_def.cpp
+++ src/libChess/doska_def.cpp
@@ -4,9 +4,8 @@
 #include <string>
 using namespace std;
 
-void doska_def(bool**gde,string**DOSKA)
+void doska_def(bool** gde, string** DOSKA)
 {
-
     for (int i = 0; i < 8; i++) {
         for (int j = 1; j < 9; j++) {
             DOSKA[i][j] = " __ ";
@@ -23,20 +22,30 @@ void doska_def(bool**gde,string**DOSKA)
 
     DOSKA[0][1] = " Bb ";
     DOSKA[0][8] = " Bb ";
+    gde[0][1] = gde[0][8] = 1;
     DOSKA[7][1] = " Bw ";
     DOSKA[7][8] = " Bw ";
+    gde[7][1] = gde[7][8] = 1;
     DOSKA[0][2] = " Nb ";
     DOSKA[0][7] = " Nb ";
+    gde[0][2] = gde[0][7] = 1;
     DOSKA[7][2] = " Nw ";
     DOSKA[7][7] = " Nw ";
+    gde[7][2] = gde[7][7] = 1;
     DOSKA[0][3] = " Rb ";
     DOSKA[0][6] = " Rb ";
+    gde[0][3] = gde[0][6] = 1;
     DOSKA[7][3] = " Rw ";
     DOSKA[7][6] = " Rw ";
+    gde[7][3] = gde[7][6] = 1;
     DOSKA[0][4] = " Qb ";
+    gde[0][4] = 1;
     DOSKA[0][5] = " Kb ";
+    gde[0][5] = 1;
     DOSKA[7][4] = " Qw ";
+    gde[7][4] = 1;
     DOSKA[7][5] = " Kw ";
+    gde[7][5] = 1;
 
     print(DOSKA);
 }
