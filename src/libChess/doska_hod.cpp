ba9a340e4006305f3081a58edf238793133a29c1
Jamesss1337 <ya_piv@mail.ru.github.com>
2021-04-30 16:06:28 +0700

    Edit doska_hod.cpp

--- src/libChess/doska_hod.cpp
+++ src/libChess/doska_hod.cpp
@@ -5,20 +5,19 @@
 #include <string>
 using namespace std;
 
-void doska_hod(bool**gde,string**buff,string**DOSKA,int x, int y)
+void doska_hod(bool** gde, string** buff, string** DOSKA, int x, int y)
 {
     if (gde[8 - y][8 - (104 - x)] == 1) {
         buff[0][0] = DOSKA[8 - y][8 - (104 - x)];
 
-        deletefigura(DOSKA,x, y);
+        deletefigura(DOSKA, x, y);
 
         gde[8 - y][8 - (104 - x)] = 0;
-        
+
     } else {
         DOSKA[8 - y][8 - (104 - x)] = buff[0][0];
 
         gde[8 - y][8 - (104 - x)] = 1;
-
 
         print(DOSKA);
     }
