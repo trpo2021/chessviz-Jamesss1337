48d59719947d2a3d6543fda4d10a9374d918df57
Jamesss1337 <ya_piv@mail.ru>
2021-05-07 23:24:51 +0700

    Creating a folder test

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+#define CTEST_MAIN
+
+#include "ctest.h"
+#include <iostream>
+#include <libChess/deletefigur.h>
+#include <libChess/doska_def.h>
+#include <libChess/gamestart.h>
+#include <libChess/doska_hod.h>
+#include <libChess/print.h>
+#include <string>
+
+using namespace std;
+
+int main(int argc, const char** argv)
+{
+    return ctest_main(argc, argv);
+}
+
